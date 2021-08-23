b424522fc42aee05399cf24c02fd7eefae884d7c
notGonim <mhmodgonim96@gmail.com>
2021-08-22 20:06:17 -1000

    feat : Result component

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { Thumbnail } from "./Thumbnail";
 
 export const Results = ({ results }) => {
-  console.log(results);
-  return <div></div>;
+  return (
+    <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+      {results.map((result) => (
+        <Thumbnail key={result.id} result={result} />
+      ))}
+    </div>
+  );
 };
-
-
